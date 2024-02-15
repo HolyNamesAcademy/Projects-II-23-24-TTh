fb887f1e78aa0380353cd72655831c881f9e7105
CEid24 <redduck8888@gmail.com>
2024-02-15 07:31:01 -0800

    Tried to get the animation of the charby hearts to work

--- frontend/src/scenes/Game.js
+++ frontend/src/scenes/Game.js
@@ -9,21 +9,42 @@ class Game extends Phaser.Scene {
   init() {}
 
   create() {
-    var charby = this.add.image(100, 100, 'charby');
-    charby.setDisplaySize(100,100);
+    this.anims.create({
+      key: 'hearts',
+      frames: this.anims.generateFrameNumbers('charbyhearts', { start: 0, end: 2 }),
+      frameRate: 4,
+    });
 
-    var pizza1 = this.add.image(20,20, 'pizza');
-    pizza1.setDisplaySize(60,60);
-    var pizza2 = this.add.image(40,20, 'pizza');
-    pizza2.setDisplaySize(60,60);
-    var pizza3 = this.add.image(60,20, 'pizza');
-    pizza3.setDisplaySize(60,60);
+    // this.keys = ['hearts'];
+    this.charbyhearts = this.add.sprite(100, 100, 'charbyhearts');
+    this.charbyhearts.setScale(8);
+
+    const charby1 = this.add.image(100, 120, 'charby');
+    charby1.setDisplaySize(200, 250);
+    // charby1.setScale(8);
+
+    const pizza1 = this.add.image(20, 20, 'pizza');
+    pizza1.setDisplaySize(60, 60);
+    const pizza2 = this.add.image(40, 20, 'pizza');
+    pizza2.setDisplaySize(60, 60);
+    const pizza3 = this.add.image(60, 20, 'pizza');
+    pizza3.setDisplaySize(60, 60);
 
     pizza3.visible = false;
   }
 
   update() {
+    const keys = ['hearts'];
+    this.charbyhearts.anims.play('hearts', true);
 
+    let c = 0;
+    this.input.on('pointerup', function (charbyhearts) {
+      c++;
+      if (c === keys.length) {
+        c = 0;
+      }
+      this.charbyhearts.anims.play(keys[c], true);
+    });
   }
 }
 
