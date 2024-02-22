2d2501a47f633a47d3ac650dcc7bd8099e799532
caitlinroach <CRoach24@holynames-sea.org>
2024-02-22 07:46:58 -0800

    adding feedbutton

--- frontend/src/components/FeedButton.jsx
+++ frontend/src/components/FeedButton.jsx
@@ -2,16 +2,14 @@ import * as React from 'react';
 import FeedButton from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
 import './FeedButton.css';
-
-function feedCharby() {}
+import FoodAlert from './FoodAlert';
 
 export default function OutlinedButtons() {
   return (
-    <Stack class="feedButton" direction="row">
-      <FeedButton variant="outlined" onClick={feedCharby}>
-          Feed Charby
+    <Stack class="feed-Button" direction="row">
+      <FeedButton variant="outlined" onClick={FoodAlert}>
+        Feed Charby
       </FeedButton>
-      {/* I don't know how to fix the indentation on this */}
     </Stack>
   );
 }
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,10 +1,12 @@
 import HomePageButton from '../components/HomePageButton';
 import PhaserGame from '../components/PhaserGame';
 import FeedButton from '../components/FeedButton';
+import FoodAlert from '../components/FoodAlert';
 
 function HomePage() {
   return (
     <div>
+      <FoodAlert />
       <PhaserGame />
       <FeedButton />
       <HomePageButton />
