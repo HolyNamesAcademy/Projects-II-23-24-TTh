f5ceb6656d20b8337fab679d5e25b6e482b6a90b
Natalie <natalieh24@outlook.com>
2024-03-12 07:27:10 -0700

    Begin welcome page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import DefaultLayout from './layouts/DefaultLayout';
 import HomePage from './pages/HomePage';
 import OtherPage from './pages/OtherPage';
+import WelcomePage from './pages/WelcomePage';
 import NoMatchPage from './pages/NoMatchPage';
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
       <Routes>
         <Route path="/" element={<DefaultLayout />}>
           <Route index element={<HomePage />} />
+          <Route path="/welcome" element={<WelcomePage />} />
           <Route path="/other" element={<OtherPage />} />
 
           {/* Using path="*"" means "match anything", so this route
---
+++ frontend/src/pages/WelcomePage.jsx
@@ -0,0 +1,26 @@
+// This is an example page component. Copy it to make
+// new pages. When you copy it, make sure to add a
+// route to the App.jsx file.
+import * as React from 'react';
+import { Button } from '@mui/material';
+
+function WelcomePage() {
+  return (
+    <>
+      <h1>Welcome to HNAmagotchi!</h1>
+      <h3>
+        In our website, users can adopt a charby, similar to a tomagotchi, 
+        and are responsible for taking care of it. This is only available
+        to students at Holy Names Academy.
+        </h3>
+      <Button href="/signup" variant="contained" color="primary">
+        Sign Up!
+      </Button>
+      <Button href="/login" variant="outlined" color="primary">
+        Log In!
+      </Button>     
+    </>
+  );
+}
+
+export default WelcomePage;
