3351a680de78614670dacb747f57e076681bbee0
caitlinroach <CRoach24@holynames-sea.org>
2024-04-09 07:33:09 -0700

    changing time to 7 seconds instead of 1.

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -43,7 +43,7 @@ function HomePage() {
         console.log('lowering hunger level', hungerLevel - 1);
         dispatch(setHungerLevel(hungerLevel - 1));
       }
-    }, 1000);
+    }, 7000);
 
     // cleanup function stops the timer when the component unmounts
     return clear;
