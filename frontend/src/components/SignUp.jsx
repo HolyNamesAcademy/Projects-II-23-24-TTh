a8d6f7f89d61f70633695af0952976d3c06b95d1
sf3990 <SFong25@holynames-sea.org>
2024-04-11 07:46:42 -0700

    a

--- frontend/src/components/SignUp.jsx
+++ frontend/src/components/SignUp.jsx
@@ -9,10 +9,6 @@ import {
   signup,
 } from '../store/user';
 
-const userApi = '/api/users/signup';
-// https://www.knowledgehut.com/blog/web-development/building-a-sign-up-form-using-react
-// https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
-// add username field
 function SignUp() {
   const [name, setName] = useState('');
   const [password, setPassword] = useState('');
@@ -20,28 +16,7 @@ function SignUp() {
   const [username, setUsername] = useState('');
   const [submitted, setSubmitted] = useState(false);
   const [error, setError] = useState(false);
-  // This function will let us dispatch changes to the store.
   const dispatch = useDispatch();
-  // Make the request to the Java API
-  // Example POST method implementation:
-  const postData = async (url = '', data = {}) => {
-    // Default options are marked with *
-    const response = await fetch(url, {
-      method: 'POST', // *GET, POST, PUT, DELETE, etc.
-      mode: 'cors', // no-cors, *cors, same-origin
-      cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-      credentials: 'same-origin', // include, *same-origin, omit
-      headers: {
-        'Content-Type': 'application/json',
-        // 'Content-Type': 'application/x-www-form-urlencoded',
-      },
-      redirect: 'follow', // manual, *follow, error
-      referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-      body: JSON.stringify(data), // body data type must match "Content-Type" header
-    });
-
-    return response.json(); // parses JSON response into native JavaScript objects
-  };
 
   // Handling the name change
   const handleName = (e) => {
@@ -65,26 +40,6 @@ function SignUp() {
     setPassword(e.target.value);
     setSubmitted(false);
   };
-
-  /**
-
-  const createUser = async () => {
-    const data = {
-      name,
-      username,
-      password,
-      email,
-    };
-
-    // eslint-disable-next-line no-console
-    console.log(data);
-
-    const user = await postData(userApi, data);
-    // eslint-disable-next-line no-console
-    console.log(user);
-  };
-
-  */
 
   const successMessage = () => (
     <div>
